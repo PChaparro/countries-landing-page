9eb4349b12b99611bf4da8168a4cb737c17477dc
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-07-08 07:04:16 -0500

    perf: Fetch the countries data from the server side
    Cache the countries data in the server to prevent multiple fetch and improve the performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,102 +1,13 @@
-"use client";
-
-import { CountryCard } from "@/components/CountryCard";
-import { Container } from "@/components/layout/Container";
+import { CountriesList } from "@/components/CountriesList";
 import { getCountries } from "@/services/countries.svc";
-import { Country } from "@/types/entities";
-import { FetchStatus } from "@/types/shared";
-import { Search } from "lucide-react";
-import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
-import { useDebounce } from "usehooks-ts";
 
-export default function Home() {
-  const { theme } = useTheme();
+export default async function Page() {
+  // Fetch the countries in the server side to cache them
+  const countries = await getCountries();
 
-  const [countries, setCountries] = useState<Country[]>([]);
-  const [fetchStatus, setFetchStatus] = useState<FetchStatus>(FetchStatus.IDLE);
-
-  const [search, setSearch] = useState<string>("");
-  const debouncedSearchField = useDebounce(search, 200);
-  const [region, setRegion] = useState<string>("");
-  const [filteredCountries, setFilteredCountries] = useState<Country[]>([]);
-
-  const fetchCountries = async () => {
-    try {
-      const countries = await getCountries();
-      setCountries(countries);
-      setFilteredCountries(countries);
-      setFetchStatus(FetchStatus.SUCCESS);
-    } catch (error) {
-      setFetchStatus(FetchStatus.ERROR);
-    }
-  };
-
-  useEffect(() => {
-    fetchCountries();
-  }, []);
-
-  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { value } = e.target;
-    setSearch(value);
-  };
-
-  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
-    const { value } = e.target;
-    setRegion(value);
-  };
-
-  useEffect(() => {
-    const filteredCountries = countries.filter((country) => {
-      const countryName = country.name.common.toLowerCase();
-      return (
-        countryName.includes(debouncedSearchField.toLowerCase()) &&
-        country.region.includes(region)
-      );
-    });
-
-    setFilteredCountries(filteredCountries);
-  }, [debouncedSearchField, region, countries]);
-
-  // TODO: Handle loading and error states
   return (
     <main>
-      <Container>
-        {/* Filter inputs */}
-        <div className="flex flex-wrap items-center justify-between my-6">
-          <div className="relative">
-            <input
-              type="text"
-              placeholder="Search for a country..."
-              className="w-full py-4 my-4 rounded-md shadow-sm md:max-w-md px-14 dark:bg-dark-soft dark:placeholder-white"
-              onChange={handleSearchChange}
-            />
-            <Search
-              size={24}
-              color={theme == "light" ? "#8C8C8C" : "white"}
-              className="absolute top-[50%] -translate-y-1/2 left-4"
-            />
-          </div>
-          <select
-            className="p-4 bg-white rounded-md shadow-sm dark:bg-dark-soft"
-            onChange={handleSelectChange}
-            defaultValue={""}
-          >
-            <option value="">Filter by region</option>
-            <option value="Africa">Africa</option>
-            <option value="Americas">Americas</option>
-            <option value="Asia">Asia</option>
-            <option value="Europe">Europe</option>
-            <option value="Oceania">Oceania</option>
-          </select>
-        </div>
-        {/* Countries container */}
-        <section className="grid content-center gap-16 mt-8 grid-cols-auto-fill-250">
-          {filteredCountries.map((country) => (
-            <CountryCard key={country.cca3} country={country} />
-          ))}
-        </section>
-      </Container>
+      <CountriesList countries={countries} />
     </main>
   );
 }
---
+++ src/components/CountriesList.tsx
@@ -0,0 +1,83 @@
+"use client";
+
+import { Search } from "lucide-react";
+import { CountryCard } from "./CountryCard";
+import { useEffect, useState } from "react";
+import { useDebounce } from "usehooks-ts";
+import { Country } from "@/types/entities";
+import { useTheme } from "next-themes";
+import { Container } from "./layout/Container";
+
+interface CountriesListProps {
+  countries: Country[];
+}
+
+export const CountriesList = ({ countries }: CountriesListProps) => {
+  const { theme } = useTheme();
+
+  const [search, setSearch] = useState<string>("");
+  const debouncedSearchField = useDebounce(search, 200);
+  const [region, setRegion] = useState<string>("");
+  const [filteredCountries, setFilteredCountries] = useState<Country[]>([]);
+
+  const handleSearchChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    setSearch(value);
+  };
+
+  const handleSelectChange = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    const { value } = e.target;
+    setRegion(value);
+  };
+
+  useEffect(() => {
+    const filteredCountries = countries.filter((country) => {
+      const countryName = country.name.common.toLowerCase();
+      return (
+        countryName.includes(debouncedSearchField.toLowerCase()) &&
+        country.region.includes(region)
+      );
+    });
+
+    setFilteredCountries(filteredCountries);
+  }, [debouncedSearchField, region, countries]);
+
+  return (
+    <Container>
+      {/* Filter inputs */}
+      <div className="flex flex-wrap items-center justify-between my-6">
+        <div className="relative">
+          <input
+            type="text"
+            placeholder="Search for a country..."
+            className="w-full py-4 my-4 rounded-md shadow-sm md:max-w-md px-14 dark:bg-dark-soft dark:placeholder-white"
+            onChange={handleSearchChange}
+          />
+          <Search
+            size={24}
+            color={theme == "light" ? "#8C8C8C" : "white"}
+            className="absolute top-[50%] -translate-y-1/2 left-4"
+          />
+        </div>
+        <select
+          className="p-4 bg-white rounded-md shadow-sm dark:bg-dark-soft"
+          onChange={handleSelectChange}
+          defaultValue={""}
+        >
+          <option value="">Filter by region</option>
+          <option value="Africa">Africa</option>
+          <option value="Americas">Americas</option>
+          <option value="Asia">Asia</option>
+          <option value="Europe">Europe</option>
+          <option value="Oceania">Oceania</option>
+        </select>
+      </div>
+      {/* Countries container */}
+      <section className="grid content-center gap-16 mt-8 grid-cols-auto-fill-250">
+        {filteredCountries.map((country) => (
+          <CountryCard key={country.cca3} country={country} />
+        ))}
+      </section>
+    </Container>
+  );
+};
