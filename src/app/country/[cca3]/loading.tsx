b9edff1edcfedbfb23b0fba12e0f017a634b5943
Pedro Andrés Chaparro Quintero <pedroandreschaparro@gmail.com>
2023-07-07 20:51:04 -0500

    feat: `/country/[code]` loading state

---
+++ src/app/country/[cca3]/loading.tsx
@@ -0,0 +1,34 @@
+"use client";
+import { BackButton } from "@/components/BackButton";
+import { Container } from "@/components/layout/Container";
+import { useTheme } from "next-themes";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+
+export default function Loading() {
+  const { theme } = useTheme();
+
+  return (
+    <SkeletonTheme
+      baseColor={theme === "light" ? "#eaeaea" : "#19252C"}
+      highlightColor={theme === "light" ? "#fcfcfc" : "#2B3743"}
+    >
+      <Container>
+        <BackButton />
+        <div className="grid items-center gap-20 my-2 md:grid-cols-2">
+          <Skeleton className="w-full aspect-[4/3]" />
+          <div className="flex flex-col justify-center gap-8">
+            <Skeleton height={40} className="w-2/3" />
+            <div className="grid gap-8 lg:grid-cols-2">
+              <div>
+                <Skeleton count={5} />
+              </div>
+              <div>
+                <Skeleton count={3} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </SkeletonTheme>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
-import { Header } from "@/components/Header";
-import "./globals.css";
-import type { Metadata } from "next";
 import { Nunito_Sans } from "next/font/google";
+import "./globals.css";
+import "react-loading-skeleton/dist/skeleton.css";
+import { Header } from "@/components/Header";
+import type { Metadata } from "next";
 import { ClientLayout } from "@/context/ClientLayout";
 
 const nunito = Nunito_Sans({ subsets: ["latin"] });
